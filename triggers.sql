a050236a5326d3ee2237e11240219deccd8781fd
Pedro Italo <pedroitl15@gmail.com>
2026-05-16 04:04:04 -0300

    add triggers

--- triggers.sql
+++ triggers.sql
@@ -2,20 +2,208 @@
 >> o AFTER INSERT em Matriculas.
 >> o Incrementa VagasOcupadas na turma.*/
 
+DELIMITER $
+CREATE TRIGGER trg_AtualizarContagemVagas 
+AFTER INSERT ON matriculas 
+FOR EACH ROW 
+BEGIN 
+	UPDATE turmas
+	SET vagas_ocupadas = vagas_ocupadas + 1 
+    WHERE id_turma = NEW.id_turma;
+END $
+DELIMITER ;
 
 /*trg_AuditoriaAluno
 o AFTER UPDATE em Alunos.
 o Caso o email seja alterado, registra em LogsSistema.*/
 
+DELIMITER $
+CREATE TRIGGER trg_AuditoriaAluno
+AFTER UPDATE ON alunos
+FOR EACH ROW
+BEGIN
+	IF OLD.email <> NEW.email THEN
+		INSERT INTO LogsSistema (id_usuario,acao, tabelaAfetada, dataHora, descricao)
+		VALUES (1,'Alteração de email','alunos',NOW(),'O email de um aluno foi alterado');
+    END IF;
+END $
+DELIMITER ;
 
 /*trg_LogOperacoesGerais
 o AFTER INSERT, UPDATE, DELETE em tabelas principais.
 o Registra ação em LogsSistema.*/
 
 
+/*alunos*/
+DELIMITER $
+CREATE TRIGGER trg_LogInsertAlunos
+AFTER INSERT ON alunos
+FOR EACH ROW
+BEGIN
+	INSERT INTO LogsSistema (id_usuario,acao, tabelaAfetada, dataHora, descricao)
+	VALUES (1,'Insert em Alunos','alunos',NOW(),'Houve uma inserção na tabela Alunos');
+END $
+DELIMITER ;
+
+DELIMITER $
+CREATE TRIGGER trg_LogUpdateAlunos
+AFTER UPDATE ON alunos
+FOR EACH ROW
+BEGIN
+	INSERT INTO LogsSistema (id_usuario,acao, tabelaAfetada, dataHora, descricao)
+	VALUES (1,'Update em Alunos','alunos',NOW(),'Houve uma atualização na tabela Alunos');
+END $
+DELIMITER ;
+
+DELIMITER $
+CREATE TRIGGER trg_LogDeleteAlunos
+AFTER DELETE ON alunos
+FOR EACH ROW
+BEGIN
+	INSERT INTO LogsSistema (id_usuario,acao, tabelaAfetada, dataHora, descricao)
+	VALUES (1,'Delete em Alunos','alunos',NOW(),'Houve uma exclusão na tabela Alunos');
+END $
+DELIMITER ;
+
+/*turmas*/
+DELIMITER $
+CREATE TRIGGER trg_LogInsertTurmas
+AFTER INSERT ON turmas
+FOR EACH ROW
+BEGIN
+	INSERT INTO LogsSistema (id_usuario,acao, tabelaAfetada, dataHora, descricao)
+	VALUES (1,'Insert em turmas','turmas',NOW(),'Houve uma inserção na tabela turmas');
+END $
+DELIMITER ;
+
+DELIMITER $
+CREATE TRIGGER trg_LogUpdateTurmas
+AFTER UPDATE ON turmas
+FOR EACH ROW
+BEGIN
+	INSERT INTO LogsSistema (id_usuario,acao, tabelaAfetada, dataHora, descricao)
+	VALUES (1,'Update em turmas','turmas',NOW(),'Houve uma atualização na tabela turmas');
+END $
+DELIMITER ;
+
+DELIMITER $
+CREATE TRIGGER trg_LogDeleteTurmas
+AFTER DELETE ON turmas
+FOR EACH ROW
+BEGIN
+	INSERT INTO LogsSistema (id_usuario,acao, tabelaAfetada, dataHora, descricao)
+	VALUES (1,'Delete em turmas','turmas',NOW(),'Houve uma exclusão na tabela turmas');
+END $
+DELIMITER ;
+
+/*professores*/
+DELIMITER $
+CREATE TRIGGER trg_LogInsertProfessores
+AFTER INSERT ON professores
+FOR EACH ROW
+BEGIN
+	INSERT INTO LogsSistema (id_usuario,acao, tabelaAfetada, dataHora, descricao)
+	VALUES (1,'Insert em professores','professores',NOW(),'Houve uma inserção na tabela professores');
+END $
+DELIMITER ;
+
+DELIMITER $
+CREATE TRIGGER trg_LogUpdateProfessores
+AFTER UPDATE ON professores
+FOR EACH ROW
+BEGIN
+	INSERT INTO LogsSistema (id_usuario,acao, tabelaAfetada, dataHora, descricao)
+	VALUES (1,'Update em professores','professores',NOW(),'Houve uma atualização na tabela professores');
+END $
+DELIMITER ;
+
+DELIMITER $
+CREATE TRIGGER trg_LogDeleteProfessores
+AFTER DELETE ON professores
+FOR EACH ROW
+BEGIN
+	INSERT INTO LogsSistema (id_usuario,acao, tabelaAfetada, dataHora, descricao)
+	VALUES (1,'Delete em professores','professores',NOW(),'Houve uma exclusão na tabela professores');
+END $
+DELIMITER ;
+
+/*cursos*/
+DELIMITER $
+CREATE TRIGGER trg_LogInsertCursos
+AFTER INSERT ON cursos
+FOR EACH ROW
+BEGIN
+	INSERT INTO LogsSistema (id_usuario,acao, tabelaAfetada, dataHora, descricao)
+	VALUES (1,'Insert em cursos','cursos',NOW(),'Houve uma inserção na tabela cursos');
+END $
+DELIMITER ;
+
+DELIMITER $
+CREATE TRIGGER trg_LogUpdateCursos
+AFTER UPDATE ON cursos
+FOR EACH ROW
+BEGIN
+	INSERT INTO LogsSistema (id_usuario,acao, tabelaAfetada, dataHora, descricao)
+	VALUES (1,'Update em cursos','cursos',NOW(),'Houve uma atualização na tabela cursos');
+END $
+DELIMITER ;
+
+DELIMITER $
+CREATE TRIGGER trg_LogDeleteCursos
+AFTER DELETE ON cursos
+FOR EACH ROW
+BEGIN
+	INSERT INTO LogsSistema (id_usuario,acao, tabelaAfetada, dataHora, descricao)
+	VALUES (1,'Delete em cursos','cursos',NOW(),'Houve uma exclusão na tabela cursos');
+END $
+DELIMITER ;
+
+/*disciplinas*/
+DELIMITER $
+CREATE TRIGGER trg_LogInsertDisciplinas
+AFTER INSERT ON disciplinas
+FOR EACH ROW
+BEGIN
+	INSERT INTO LogsSistema (id_usuario,acao, tabelaAfetada, dataHora, descricao)
+	VALUES (1,'Insert em disciplinas','disciplinas',NOW(),'Houve uma inserção na tabela disciplinas');
+END $
+DELIMITER ;
+
+DELIMITER $
+CREATE TRIGGER trg_LogUpdateDisciplinas
+AFTER UPDATE ON disciplinas
+FOR EACH ROW
+BEGIN
+	INSERT INTO LogsSistema (id_usuario,acao, tabelaAfetada, dataHora, descricao)
+	VALUES (1,'Update em disciplinas','disciplinas',NOW(),'Houve uma atualização na tabela disciplinas');
+END $
+DELIMITER ;
+
+DELIMITER $
+CREATE TRIGGER trg_LogDeleteDisciplinas
+AFTER DELETE ON disciplinas
+FOR EACH ROW
+BEGIN
+	INSERT INTO LogsSistema (id_usuario,acao, tabelaAfetada, dataHora, descricao)
+	VALUES (1,'Delete em disciplinas','disciplinas',NOW(),'Houve uma exclusão na tabela disciplinas');
+END $
+DELIMITER ;
+
 /*trg_AtualizarHistoricoAutomaticamente
 o AFTER UPDATE em Matriculas.
 o Se o status mudar para 'Aprovado', insere no HistoricoAluno.*/
+
+DELIMITER $
+CREATE TRIGGER trg_AtualizarHistoricoAutomaticamente
+AFTER UPDATE ON matriculas
+FOR EACH ROW
+BEGIN
+	IF OLD.status <> NEW.status AND NEW.status = 'Aprovado' THEN
+	INSERT INTO historicoAluno (id_aluno, id_disciplina, notaFinal, status, dataConclusao)
+	VALUES (NEW.id_aluno, (SELECT id_disciplina FROM turmas WHERE id_turma = NEW.id_turma), NEW.nota_final, 'Aprovado', NOW());
+    END IF;
+END $
+DELIMITER ;
 
 /*trg_AtualizarStatusAutomaticamente
 >> o AFTER UPDATE em Matriculas
@@ -24,24 +212,12 @@ matricular em uma nova disciplina, o sistema deve impedir a matrícula e
 registrar o evento em LogsSistema.
 */
 
-/*Valida CPF e salva no formato correto*/
-
-DELIMITER $$
-
-CREATE TRIGGER trg_valida_formata_cpf
-BEFORE INSERT ON alunos
+DELIMITER $
+CREATE TRIGGER trg_AtualizarStatusAutomaticamente
+AFTER UPDATE ON matriculas
 FOR EACH ROW
 BEGIN
-    SET NEW.cpf = REPLACE(REPLACE(NEW.cpf, '.', ''), '-', '');
-    IF NEW.cpf NOT REGEXP '^[0-9]{11}$' THEN
-        SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'CPF inválido.';
+	IF (SELECT id_disciplina FROM turmas WHERE id_turma = NEW.id_turma
     END IF;
-    SET NEW.cpf = CONCAT(
-        SUBSTRING(NEW.cpf,1,3), '.', 
-        SUBSTRING(NEW.cpf,4,3), '.', 
-        SUBSTRING(NEW.cpf,7,3), '-', 
-        SUBSTRING(NEW.cpf,10,2)
-    );
-END$$
+END $
 DELIMITER ;
